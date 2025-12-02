f3537c51cef568e67d6ed288e5e8fac4ccb0e05a
Michael Cortese <michael.cortese@ryerson.ca>
2025-12-02 07:51:28 -0500

    aoc2025 - day 1 part 2

--- aoc_2025/src/day_01/day1.rs
+++ aoc_2025/src/day_01/day1.rs
@@ -1,4 +1,4 @@
-pub fn part1(input: &str) -> u32 {
+pub fn part1(input: &str) -> i32 {
     let mut dial = 50;
     let mut code = 0;
 
@@ -13,19 +13,70 @@ pub fn part1(input: &str) -> u32 {
     code
 }
 
-// Takes the dial and the next rotation. Returns the dial rotated by rotation.
-fn rotate_dial(mut dial: u32, rotation: &str) -> u32 {
-    // Assume every dial has 100 values
-    let (direction, value) = rotation.split_at(1);
-    let value = value.parse::<u32>().expect("expected a number");
+pub fn part2(input: &str) -> i32 {
+    let mut dial = 50;
+    let mut code = 0;
 
-    if direction == "L" {
-        dial = dial + 100 - (value % 100);
-    } else {
-        dial += value;
+    let rotations: Vec<&str> = input.trim().split('\n').collect();
+    for rot in rotations {
+        let count;
+        (dial, count) = count_rotations(dial, rot);
+        code += count;
     }
 
-    dial % 100
+    code
+}
+
+// Takes the dial and the next rotation. Returns the dial rotated by rotation.
+fn rotate_dial(dial: i32, rotation: &str) -> i32 {
+    // Assume every dial has 100 values
+    let (direction, value) = rotation.split_at(1);
+    let mut value = value.parse::<i32>().expect("expected a number");
+
+    if direction == "L" {
+        value = -value;
+    }
+
+    (dial + value) % 100
+}
+
+// Takes the dial and the next rotation. Returns the dial rotated by rotation and the number of
+// times the dial touched 0.
+fn count_rotations(mut dial: i32, rotation: &str) -> (i32, i32) {
+    // Assume every dial has 100 values
+    let (direction, value) = rotation.split_at(1);
+    let mut value = value.parse::<i32>().expect("expected a number");
+    let mut count = 0;
+
+    if value >= 100 {
+        count = (value / 100).abs();
+        value %= 100;
+    }
+
+    if direction == "L" {
+        if dial == 0 {
+            dial -= value;
+            if dial < 0 {
+                dial += 100;
+            }
+        } else {
+            dial -= value;
+            if dial <= 0 {
+                count += 1;
+                if dial < 0 {
+                    dial += 100;
+                }
+            }
+        }
+    } else {
+        dial += value;
+        if dial >= 100 {
+            count += 1;
+            dial -= 100;
+        }
+    }
+
+    (dial, count)
 }
 
 #[cfg(test)]
@@ -36,5 +87,11 @@ mod tests {
     fn part1_example() {
         let input = include_str!("input/test_input.txt");
         assert_eq!(3, part1(input));
+    }
+
+    #[test]
+    fn part2_example() {
+        let input = include_str!("input/test_input.txt");
+        assert_eq!(6, part2(input));
     }
 }
--- aoc_2025/src/main.rs
+++ aoc_2025/src/main.rs
@@ -3,8 +3,13 @@ use crate::day_01::*;
 mod day_01;
 
 fn main() {
-    print!(
+    println!(
         "Day1 part1: {}",
         day1::part1(include_str!("day_01/input/input.txt"))
+    );
+
+    println!(
+        "Day1 part2: {}",
+        day1::part2(include_str!("day_01/input/input.txt"))
     );
 }
