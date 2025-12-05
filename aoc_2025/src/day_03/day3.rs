edde70b33ebe1a11ed066b265b3b36447d64aad9
Cortese, Michael <Michael.Cortese@rbbn.com>
2025-12-05 13:22:58 -0500

    aoc2025 - day 3 part2 - completed

--- aoc_2025/src/day_03/day3.rs
+++ aoc_2025/src/day_03/day3.rs
@@ -1,69 +1,57 @@
 pub fn part1(input: &str) -> u64 {
-    input
-        .trim()
-        .split('\n')
-        .map(|bank| {
-            let (f, s) = top_two(bank);
-            f * 10 + s
-        })
-        .sum()
+    input.trim().split('\n').map(|bank| top_k(bank, 2)).sum()
 }
 
 pub fn part2(input: &str) -> u64 {
-    input.trim().split('\n').map(|bank| topk(bank, 12)).sum()
+    input.trim().split('\n').map(|bank| top_k(bank, 12)).sum()
 }
 
-fn top_two(numbers: &str) -> (u64, u64) {
+// Takes in a string of numbers and the number of elements that need to be kept -> returns the
+// largest number made up of k elements in the initial list stably
+fn top_k(numbers: &str, mut k: usize) -> u64 {
+    // We need to always choose the first largest number in the front range of the list, until we
+    // have chosed k, or only k remain in the list.
     let numbers: Vec<u64> = numbers
         .chars()
         .map(|c| c.to_digit(10).unwrap() as u64)
         .collect();
 
-    let mut first = numbers.first().unwrap();
-    let mut index = 0;
-    for i in 1..numbers.len() - 1 {
+    let mut start = 0;
+    let mut sum = 0;
+    // if k == 0 we found all numbers, if len - k == 0, we should take the remaining numbers
+    while k != 0 && numbers.len() - k != 0 {
+        let end = numbers.len() - k;
+        // get the largest number in the current range we're considering.
+        let (index, num) = top_in_range(&numbers, start, end);
+        start = index + 1;
+        sum *= 10;
+        sum += num;
+        k -= 1;
+    }
+
+    while k != 0 {
+        sum *= 10;
+        sum += numbers.get(numbers.len() - k).unwrap();
+        k -= 1;
+    }
+
+    sum
+}
+
+// Takes a list of numbers, start and end index of a range and returns the largest number and it's
+// index in the list.
+fn top_in_range(numbers: &[u64], start: usize, end: usize) -> (usize, u64) {
+    let (mut index, mut largest) = (start, numbers.get(start).unwrap());
+
+    for i in (start + 1)..=end {
         let num = numbers.get(i).unwrap();
-        if num > first {
-            first = num;
+        if num > largest {
+            largest = num;
             index = i;
         }
     }
 
-    let mut second = numbers.get(index + 1).unwrap();
-    for i in (index + 2)..numbers.len() {
-        let num = numbers.get(i).unwrap();
-        if num > second {
-            second = num;
-        }
-    }
-
-    (*first, *second)
-}
-
-fn topk(numbers: &str, k: usize) -> u64 {
-    let mut numbers: Vec<(usize, u64)> = numbers
-        .chars()
-        .enumerate()
-        .map(|(i, c)| (i, c.to_digit(10).unwrap() as u64))
-        .collect();
-
-    numbers.sort_by(|a, b| b.1.cmp(&a.1));
-    // numbers.sort_by_key(|(_, num)| *num);
-    println!("{:?}", numbers);
-
-    // now take the topk and sort by index
-    let mut numbers: Vec<(usize, u64)> = numbers.into_iter().take(k).collect();
-    numbers.sort_by_key(|(index, _)| *index);
-    println!("{:?}", numbers);
-
-    let mut combined = numbers.first().unwrap().1;
-    for i in 1..numbers.len() {
-        combined *= 10;
-        combined += numbers.get(i).unwrap().1;
-    }
-    println!("{combined}");
-
-    combined
+    (index, *largest)
 }
 
 #[cfg(test)]
