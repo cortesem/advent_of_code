3075a5d984ee81ec0cd777b6766530e38710c8a5
Michael Cortese <michael.cortese@ryerson.ca>
2025-12-13 21:40:54 -0500

    aoc2025 - day 8 part 2

--- aoc_2025/src/day_08/day8.rs
+++ aoc_2025/src/day_08/day8.rs
@@ -55,7 +55,29 @@ pub fn part1(input: &str, num_of_conn: usize) -> u64 {
 }
 
 pub fn part2(input: &str) -> u64 {
-    1
+    // get all positions
+    let mut locations: Vec<Position> = vec![];
+    for location in input.lines() {
+        let xyz: Vec<&str> = location.split(',').collect();
+        locations.push(Position(
+            xyz[0].parse().unwrap(),
+            xyz[1].parse().unwrap(),
+            xyz[2].parse().unwrap(),
+        ));
+    }
+
+    // calculate distances for each position
+    let mut positions_with_distance = Vec::<Vec<Connection>>::new();
+    for position in locations.iter() {
+        positions_with_distance.push(calc_distances(position, &locations));
+    }
+    let mut positions_with_distance: Vec<Connection> =
+        positions_with_distance.into_iter().flatten().collect();
+    // this might contain duplicates when two points are both closest to eachother.
+    positions_with_distance.sort_by(|a, b| a.2.total_cmp(&b.2));
+    positions_with_distance.dedup();
+
+    create_full_circuit(&positions_with_distance, locations.len())
 }
 
 // calculate distances between a position and every other position. Returns a list of distances to
@@ -119,6 +141,48 @@ fn get_n_closest_connections(
     result
 }
 
+fn create_full_circuit(connections: &[Connection], num_of_junctions: usize) -> u64 {
+    let mut result = Vec::<HashSet<Position>>::new();
+
+    for connection in connections.iter() {
+        let mut found = false;
+        for circuit in result.iter_mut() {
+            if circuit.contains(&connection.0) || circuit.contains(&connection.1) {
+                found = true;
+                circuit.insert(connection.0.clone());
+                circuit.insert(connection.1.clone());
+                break;
+            }
+        }
+
+        if !found {
+            let mut circuit = HashSet::new();
+            circuit.insert(connection.0.clone());
+            circuit.insert(connection.1.clone());
+            result.push(circuit);
+        } else {
+            // check for any circuits that have an intersection
+            for i in 0..result.len() - 1 {
+                for j in (i + 1)..result.len() {
+                    if !result.get(i).unwrap().is_disjoint(result.get(j).unwrap()) {
+                        let b = result.remove(j);
+                        let mut a = result.remove(i);
+                        a.extend(b);
+                        result.push(a);
+                        break;
+                    }
+                }
+            }
+        }
+        // Now we need to check if we have a single circuit of 1000
+        if result.len() == 1 && result.first().unwrap().len() == num_of_junctions {
+            return (connection.0.0 * connection.1.0) as u64;
+        }
+    }
+
+    1
+}
+
 #[cfg(test)]
 mod tests {
     use crate::day_08::day8::*;
@@ -132,6 +196,6 @@ mod tests {
     #[test]
     fn part2_example() {
         let input = include_str!("input/test_input.txt");
-        assert_eq!(1, part2(input));
+        assert_eq!(25272, part2(input));
     }
 }
